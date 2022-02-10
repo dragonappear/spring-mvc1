8add31c6d9057d2254790c0ef4fab8c213954c00
dragonappear <dragonappear1@gmail.com>
2022-02-10 17:02:55 +0900

    ModelAttribute Imple

--- src/main/java/me/dragonappear/springmvc1/request/requestparam/ModelAttributeController.java
+++ src/main/java/me/dragonappear/springmvc1/request/requestparam/ModelAttributeController.java
@@ -1,6 +1,7 @@
 package me.dragonappear.springmvc1.request.requestparam;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ModelAttributeController {
 
     // 스프링에서 자동으로 객체를 생성해서 프로퍼티에 값을 넣어준다.
+    // 그리고 model.addAttribute("modelAttributeDto",dto)를 실행해준다.
     @RequestMapping("/v1")
     public String v1(@ModelAttribute ModelAttributeDto dto) {
         log.info("username:{},age:{}", dto.getUsername(),dto.getAge());
@@ -18,6 +20,7 @@ public class ModelAttributeController {
     }
 
     // 스프링에서 자동으로 객체를 생성해서 프로퍼티에 값을 넣어준다.
+    // @ModelAttribute를 생략해도 model.addAttribute("modelAttributeDto",dto)를 실행해준다.
     @RequestMapping("/v2")
     public String v2(ModelAttributeDto dto) {
         log.info("username:{},age:{}", dto.getUsername(),dto.getAge());
